47e5eba5d5843e8a5e350590c3627648e85dcd41
JN-Networks <mbyix8qa@jixten.de>
2025-03-14 14:35:15 +0100

    Update search_files.ps1

--- search_files.ps1
+++ search_files.ps1
@@ -1,21 +1,22 @@
 Write-Host "Please insert your path:" -ForegroundColor Yellow
-$path = Read-Host "path" 
+$path = Read-Host "path"
 
 Write-Host "Please insert your searchtext:" -ForegroundColor Yellow
 $searchtext = Read-Host "searchtext"
 
-$files = Get-ChildItem -Path $Pfad -File -Recurse
+# Dateien rekursiv sammeln
+$files = Get-ChildItem -Path $path -File -Recurse
 
 $results = @()
 
-foreach ($files in $file) {
-    $pointer = Select-String -Path $files.FullName -Pattern $Suchtext
-    
-    foreach ($pointerline in $pointer) {
+foreach ($file in $files) {
+    $matches = Select-String -Path $file.FullName -Pattern $searchtext
+
+    foreach ($match in $matches) {
         $results += [PSCustomObject]@{
-            "Filename" = $files.Name
-            "Path" = $files.FullName
-            "Pointer Line" = $pointerline.LineNumber
+            "Filename"     = $file.Name
+            "Path"         = $file.FullName
+            "Pointer Line" = $match.LineNumber
         }
     }
 }
@@ -23,5 +24,5 @@ foreach ($files in $file) {
 if ($results.Count -gt 0) {
     $results | Format-Table -AutoSize
 } else {
-    Write-Host "No results find." -ForegroundColor Red
+    Write-Host "No results found." -ForegroundColor Red
 }
